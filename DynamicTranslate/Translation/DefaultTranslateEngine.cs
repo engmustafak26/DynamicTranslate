c81a8aa8d0d09b8094940b402c73fa359217d67c
mustafa kamal <mkamal@sejeltech.com>
2024-10-19 08:48:40 +0300

    fix translation error

--- DynamicTranslate/Translation/DefaultTranslateEngine.cs
+++ DynamicTranslate/Translation/DefaultTranslateEngine.cs
@@ -1,4 +1,5 @@
-﻿using GTranslate.Translators;
+﻿using GTranslate.Results;
+using GTranslate.Translators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +19,18 @@ namespace DynamicTranslate.Translation
             {
                 if (input is null || input.Length == 0)
                     return new string[0];
-                const string delimiter = @" /\ ";
+                const string delimiter = @" ____ ";
 
                 var translator = new GoogleTranslator();
 
-                var result = await translator.TranslateAsync(string.Join(delimiter, input), toLanguageCode, fromLanguageCode);
-                return result.Translation.Split(new string[] { delimiter }, StringSplitOptions.RemoveEmptyEntries);
+                List<Task<GoogleTranslationResult>> tasks = new List<Task<GoogleTranslationResult>>(input.Length);
+                foreach (var token in input)
+                {
+                    tasks.Add(translator.TranslateAsync(token, toLanguageCode, fromLanguageCode));
+                }
+                await Task.WhenAll(tasks);
+                var result = tasks.Select(x => x.Result.Translation).ToArray();
+                return result;
             }
             catch (Exception ex)
             {
